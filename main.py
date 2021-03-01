8ef7aa93a7a833761be61e93e2e81fd80b0e9240
Rodrigo Soares <rodrigosoares@alu.ufc.br>
2021-03-01 17:34:47 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import  glob
+
+Dez_20 = pd.read_csv('//C:/Users/rodrigo.soares/, sep=';')
+# TODO Find path '//Trade de Marketing/Inteligencia/BANCO_DE_DADOS/RODRIGO/ANS/2020/12/ben202012_AC.csv'
+
+
