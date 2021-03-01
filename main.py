a66f5e3eff9ab8117470cf58b3db5a207cf87f20
Rodrigo Soares <rodrigosoares@alu.ufc.br>
2021-03-01 18:36:07 -0300

    dia 1/3 final do dia

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import  glob
 
-Dez_20 = pd.read_csv('//C:/Users/rodrigo.soares/, sep=';')
-# TODO Find path '//Trade de Marketing/Inteligencia/BANCO_DE_DADOS/RODRIGO/ANS/2020/12/ben202012_AC.csv'
+Dez_20 = pd.DataFrame()
+Dez_20 = pd.read_csv('//10.1.17.4/Setores/Trade de Marketing/Inteligencia/BANCO_DE_DADOS/RODRIGO/ANS/2020/12/ben202012_AC.csv', sep=';')
+# TODO Find path '//10.1.17.4/Setores/Trade de Marketing/Inteligencia/BANCO_DE_DADOS/RODRIGO/ANS/2020/12/ben202012_AC.csv'
 
 
+if __name__ == '__main__':
+    print(Dez_20)
